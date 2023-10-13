7816ec4d63f53f383baba6dc3515fee357e28cb6
Hands-on-robotics <handsonevanoff@gmail.com>
2023-10-13 16:08:13 -0700

    created tweet template

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  
+  const $textarea = $('.new-tweet textarea');
+
+  $textarea.on('input', function() {
+    const $counter = $('.counter');
+    const inputLength = $(this).val().length;
+    const maxCharacters = 140;
+    const characterCounter = maxCharacters - inputLength;
+
+    if (characterCounter < 0) {
+      $counter.css('color', 'red');
+    } else if (characterCounter >= 0) {
+      $counter.css('color', '#545149');
+    }
+    $counter.text(characterCounter);
+  });
 });
