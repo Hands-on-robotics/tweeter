563a8593996b4850c0568947283c20de36f1672e
Hands-on-robotics <handsonevanoff@gmail.com>
2023-10-21 14:44:31 -0700

    Added a slide up and down error message

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,65 +5,80 @@
  */
 
 // Functions //
-
-const createTweetElement = function(tweetData) {
-  let $tweet = `
+$(document).ready(function() {
+  const createTweetElement = function(tweetData) {
+    const escape = function (str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
+    
+    const $tweet = `
     <article class="tweet">
-      <header class="tweet-header">
-        <img src="${tweetData.user.avatars}">
-        <div class="user-info">
-          <div class="username">${tweetData.user.name}</div>
-          <div class="user-tag">${tweetData.user.handle}</div>
-        </div>
-      </header>
-      <p>${tweetData.content.text}</p>
-      <footer>
-        <span class="date template">${timeago.format(tweetData.created_at)}</span>
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </footer>
+    <header class="tweet-header">
+    <img src="${escape(tweetData.user.avatars)}">
+    <div class="user-info">
+    <div class="username">${escape(tweetData.user.name)}</div>
+    <div class="user-tag">${escape(tweetData.user.handle)}</div>
+    </div>
+    </header>
+    <p class="tweet-text">${escape(tweetData.content.text)}</p>
+    <footer>
+    <span class="date template">${timeago.format(tweetData.created_at)}</span>
+    <i class="fa-solid fa-flag"></i>
+    <i class="fa-solid fa-retweet"></i>
+    <i class="fa-solid fa-heart"></i>
+    </footer>
     </article>`;
-
-  return $tweet;
-};
-
-const renderTweets = function(tweets) {
-  tweets.forEach((tweet) => {
-    $('.tweets-container').prepend(createTweetElement(tweet));
-  });
-};
-
-const loadTweets = function() {
-  $.ajax("/tweets", { method: "GET" })
-    .then((response) => {
-      renderTweets(response);
-    });
-};
+    
+    return $tweet;
+  };
   
-loadTweets();
-
-// Routes //
-
-$('#tweet-form').on('submit', (event) => {
-  event.preventDefault();
-  const tweetText = $('#tweet-text').val();
-
-  if (tweetText.length > 0 && tweetText.length < 140) {
-    // CODE REVIEW: The form does not clear when successfully submitted
-    $.post({
-      method: "POST",
-      url: "/tweets",
-      // CODE REVIEW: 
-      data: $('#tweet-form').serialize(),
-    })
-    .then((res) => {
+  // CODE REVIEW: best practices for preventing code injection
+  const renderTweets = function(tweets) {
+    tweets.forEach((tweet) => {
+      const $tweet = createTweetElement(tweet);
+      $('.tweets-container').prepend($tweet);
+    });
+  };
+  
+  const loadTweets = function() {
+    $.ajax("/tweets", { method: "GET" })
+      .then((response) => {
+        renderTweets(response);
+      });
+  };
+  
+  loadTweets();
+  
+  // Routes //
+  
+  // post tweet
+  $('#tweet-form').on('submit', (event) => {
+    event.preventDefault();
+    $('.error');
+    
+    const tweetText = $('#tweet-text').val();
+    
+    if (tweetText.length > 0 && tweetText.length < 140) {
+      // CODE REVIEW: The form does not clear when successfully submitted
+      $.post({
+        method: "POST",
+        url: "/tweets",
+        data: $('#tweet-form').serialize(),
+      })
+      .then((res) => {
         loadTweets();
         $('#tweet-form')[0].reset();
-      });
-  } else if (tweetText.length > 140) {
-    alert('This tweet is too long to post.');
-  } else if (tweetText.length < 1) {
-    alert('Would you like to type something first?');
-  }
+        $('.error').slideUp();
+        });
+    } else if (tweetText.length > 140) {
+      $('.error p').text('This tweet is too long to post.');
+      $('.error').slideDown(450);
+    } else if (tweetText.length < 1) {
+      $('.error p').text('Would you like to type something first?');
+      $('.error').slideDown(450);
+    }
+  });
+  
 });
