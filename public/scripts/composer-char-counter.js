1ca2d37e551ac9ec017f16f3b27e09f5caacf05f
Hands-on-robotics <handsonevanoff@gmail.com>
2023-10-25 18:57:25 -0700

    fixed counter disappearing bug

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,34 @@
-$(document).ready(function() {
+// $(document).ready(function() {
   
-  $('.new-tweet textarea').on('input', function() {
-    const maxCharacters = 140;
-    const $counter = $('.counter');
-    const inputLength = $(this).val().length;
-    const characterCounter = maxCharacters - inputLength;
+//   $('.new-tweet textarea').on('input', function() {
+//     const maxCharacters = 140;
+//     const counter = $('.counter');
+//     const inputLength = $(this).val().length;
+//     const characterCounter = maxCharacters - inputLength;
 
-    if (characterCounter < 0) {
-      $counter.addClass('.error');
-    } else if (characterCounter >= 0) {
-      $counter.removeClass('.error');
+//     if (characterCounter < 0) {
+//       counter.addClass('counter-error');
+//     } else if (characterCounter >= 0) {
+//       counter.removeClass('counter-error');
+//     }
+//     counter.text(characterCounter);
+//   });
+// });
+
+$(document).ready(function () {
+  $(".new-tweet textarea").on("input", function () {
+    const maxLength = 140;
+    const textArea = $(this);
+    const currentLength = textArea.val().length;
+    const remainingChar = maxLength - currentLength;
+    const counter = textArea.parent().find(".counter");
+    
+    counter.text(remainingChar);
+
+    if (remainingChar < 0) {
+      counter.addClass("counter-error");
+    } else {
+      counter.removeClass("counter-error");
     }
-    $counter.text(characterCounter);
   });
 });
